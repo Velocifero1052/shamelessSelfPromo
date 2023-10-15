ecbc6f04d4e01dad40f63593ac9e415a8c4902bf
Velocifero1052 <commelevent1052@yandex.ru>
2023-10-16 00:45:21 +0500

    preview submission opens sms send function and button setup completed

--- app/src/main/java/com/example/self_promoution/PreviewActivity.kt
+++ app/src/main/java/com/example/self_promoution/PreviewActivity.kt
@@ -1,20 +1,27 @@
 package com.example.self_promoution
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_preview.button_send_message
 import kotlinx.android.synthetic.main.activity_preview.text_view_message
 
 class PreviewActivity : AppCompatActivity() {
 
-    private var textViewMessage: TextView? = null
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+        displayMessage()
+        setupButton()
+    }
 
-        val message: Message = intent.getSerializableExtra("message") as Message
-        val testString = """
+    private fun displayMessage() {
+        message = intent.getSerializableExtra("message") as Message
+        messagePreviewText = """
                Hi ${message.contactName},
                
                My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
@@ -28,6 +35,17 @@ class PreviewActivity : AppCompatActivity() {
                Thanks and best regards.
             """.trimIndent()
 
-        text_view_message.text = testString
+        text_view_message.text = messagePreviewText
     }
+
+    private fun setupButton() {
+        button_send_message.setOnClickListener {
+            val sendIntent: Intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto: ${message.contactNumber}")
+                putExtra("sms_body", messagePreviewText)
+            }
+            startActivity(sendIntent)
+        }
+    }
+
 }
